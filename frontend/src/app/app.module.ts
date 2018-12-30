19eabe745014f1e4f7bffec97e61be99245f8389
Anurag Mishra <anurag8m@gmail.com>
2018-12-30 19:34:02 +0530

    Logout and CanActivate Service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HttpClientModule } from "@angular/common/http";
 import { JarwisService } from "./Services/jarwis.service";
 import { TokenService } from "./Services/token.service";
 import { AuthService } from "./Services/auth.service";
+import { AfterLoginService } from "./Services/after-login.service";
+import { BeforeLoginService } from "./Services/before-login.service";
 
 @NgModule({
   declarations: [
@@ -26,7 +28,13 @@ import { AuthService } from "./Services/auth.service";
     ResponseResetComponent
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
-  providers: [JarwisService, TokenService, AuthService],
+  providers: [
+    JarwisService,
+    TokenService,
+    AuthService,
+    AfterLoginService,
+    BeforeLoginService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
