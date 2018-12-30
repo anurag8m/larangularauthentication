9d60a6edcd5b0a51754b6c178de62ba79076143b
Anurag Mishra <anurag8m@gmail.com>
2018-12-30 15:13:22 +0530

    laravel signup with form validation

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.scss"]
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public form = {
+    email: null,
+    name: null,
+    password: null,
+    password_confirmation: null
+  };
+  public error = [];
+  constructor(private http: HttpClient) {}
+  onSubmit() {
+    return this.http
+      .post("http://localhost:8000/api/signup", this.form)
+      .subscribe(data => console.log(data), error => this.handleError(error));
   }
-
+  handleError(error) {
+    this.error = error.error.errors;
+  }
+  ngOnInit() {}
 }
